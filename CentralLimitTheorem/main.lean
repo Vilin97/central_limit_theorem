39e034ee0c56f25bc9206b8982b81e88d0820da1
Siyuan Ge <jamesgsy@cs.washington.edu>
2024-10-31 15:58:11 -0700

    update the calc steps for the proof of lemma 2

--- CentralLimitTheorem/main.lean
+++ CentralLimitTheorem/main.lean
@@ -16,13 +16,21 @@ open MeasureTheory ProbabilityTheory
 
 variable [MeasurableSpace Ω] (μ: Measure Ω) [IsProbabilityMeasure μ]
 variable (X: Ω → ℝ)
-variable (hX: (Measure.map X μ) = (gaussianReal 0 1))
 -- TODO: Assert that X is measurable
 
-
-theorem mgf_of_gaussian: ∀ t : ℝ, mgf X μ t = Real.exp (t ^ 2 / 2) := by
+theorem mgf_of_gaussian (hX: (Measure.map X μ) = (gaussianReal 0 1)) :
+  ∀ t : ℝ, mgf X μ t = Real.exp (t ^ 2 / 2) := by
     intro t
     simp [mgf]
-    sorry
+    calc
+      mgf X μ t = μ[fun w => Real.exp ((X w) * t)] := by sorry
+      _ = (Measure.map X μ)[fun x => Real.exp (x * t)] := by sorry
+      _ = ∫ (x : ℝ), Real.exp (t * x) * (gaussianPDFReal 0 1 x) := by sorry
+      _ = ∫ (x : ℝ), Real.exp (t * x) * (√(2 * Real.pi))⁻¹ * Real.exp (- (x ^ 2) / 2) := by sorry
+      _ = ∫ (x : ℝ), (√(2 * Real.pi))⁻¹ * Real.exp (t * x - (x ^ 2) / 2) := by sorry
+      _ = Real.exp (t ^ 2/ 2) * ∫ (x : ℝ), (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2) := by sorry
+      _ = Real.exp (t ^ 2 / 2) := by sorry
+
+#check mgf_of_gaussian
 
 -- #check ProbabilityTheory.mgf fun x => ((ProbabilityTheory.gaussianReal 0 1).measureOf x).toReal (ProbabilityTheory.gaussianReal 0 1)
