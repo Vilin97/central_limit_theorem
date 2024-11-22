f3161729625c0b6e353675f4b0b8ec59b7a90f3f
Vas <vasilin97@gmail.com>
2024-11-21 16:32:24 -0800

    State CLT.

--- CentralLimitTheorem/main.lean
+++ CentralLimitTheorem/main.lean
@@ -1,6 +1,7 @@
 import Mathlib.Probability.Moments
 import Mathlib.Probability.Distributions.Gaussian
 import Mathlib.Data.Complex.Exponential
+import Mathlib.Probability.StrongLaw
 
 #check ProbabilityTheory.mgf
 #check ProbabilityTheory.gaussianReal
@@ -79,4 +80,16 @@ theorem mgf_of_gaussian (hXm: Measurable X) (hX: (Measure.map X μ) = (gaussianR
 #check mgf_of_gaussian
 
 
-theorem CLT {Y : ι → Ω → ℝ} (h_meas : ∀ (i : ι), Measurable (Y i)) (h_indep : ProbabilityTheory.iIndepFun (fun (i : ι) => inferInstance) Y μ) (h_identically_distributed : true) (h_zero_mean : true) (h_unit_variance : true) (h_gaussian: (Measure.map X μ) = (gaussianReal 0 1)) : ∀ t : ℝ, (μ {w : (Y i) w ≤ t}).tendsto μ {w : (X i) w ≤ t} := sorry
+variable {Y : ℕ → Ω → ℝ}
+#check ∑ i ≤ 2, Y i
+
+theorem CLT
+(h_meas : ∀ (i : ℕ), Measurable (Y i)) -- measurable
+(h_indep : ProbabilityTheory.iIndepFun (fun (i : ℕ) => inferInstance) Y μ) -- independent
+(hident : ∀ (i : ℕ), ProbabilityTheory.IdentDistrib (Y i) (Y 0) μ μ) -- identically distributed
+(h_zero_mean : ∀ i, μ[Y i] = 0) -- zero mean
+(h_unit_variance : ∀ i, μ[(Y i)^2] = 1) -- unit variance
+(h_gaussian: (Measure.map X μ) = (gaussianReal 0 1)) -- limit is standard Gaussian
+: ∀ t : ℝ, Filter.Tendsto
+  (fun (n : ℕ) => (μ {w | ∑ i ∈ Finset.range n, (Y i w)/(Real.sqrt n) ≤ t}) - (μ {w | X w ≤ t})) Filter.atTop (nhds 0) := by -- converge in distribution
+  sorry
