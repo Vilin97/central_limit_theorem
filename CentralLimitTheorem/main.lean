1b5582774c34c168a58b0cd37ab5c3d5122ed95d
Vas <vasilin97@gmail.com>
2024-11-26 16:25:29 -0800

    Finish lemma 3 from the blueprint.

--- CentralLimitTheorem/main.lean
+++ CentralLimitTheorem/main.lean
@@ -30,21 +30,7 @@ theorem CLT
   (fun (n : ℕ) => (μ {w | ∑ i ∈ Finset.range n, (Y i w)/(Real.sqrt n) ≤ t}) - (μ {w | X w ≤ t})) Filter.atTop (nhds 0) := by -- converge in distribution
   sorry
 
-lemma mgf_of_gaussian_integral_eq: (fun x => Real.exp (t * x) * ((√(2 * Real.pi))⁻¹ * Real.exp (- (x ^ 2) / 2))) =  (fun x => Real.exp (t ^ 2/ 2) * (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2)) := by
-  ext x
-  simp [mul_sub, sub_eq_zero, mul_assoc, mul_comm, mul_left_comm, ← Real.exp_sub, ← Real.exp_add]
-  field_simp [Real.exp_ne_zero]
-  rw [← Real.exp_add, ← Real.exp_add]
-  field_simp
-  ring
-
-lemma mgf_of_gaussian_integral_eq₂:  (fun x => (gaussianPDFReal 0 1 x).toNNReal • Real.exp (t * x)) = (fun x => Real.exp (t * x) * gaussianPDFReal 0 1 x) := by
-  ext x
-  rw [NNReal.smul_def]
-  rw [Real.coe_toNNReal (gaussianPDFReal 0 1 x) (gaussianPDFReal_nonneg 0 1 x)]
-  rw [mul_comm (Real.exp (t * x)) (gaussianPDFReal 0 1 x)]
-  rfl
-
+-- MGF of standard gaussian is exp(t^2/2)
 theorem mgf_of_gaussian (X: Ω → ℝ) (hXm: Measurable X) (hX: (Measure.map X μ) = (gaussianReal 0 1)) :
   ∀ t : ℝ, mgf X μ t = Real.exp (t ^ 2 / 2) := by
     intro t
@@ -66,13 +52,26 @@ theorem mgf_of_gaussian (X: Ω → ℝ) (hXm: Measurable X) (hX: (Measure.map X 
         rw [gaussianPDF_def]
         simp [ENNReal.ofReal]
         rw [integral_withDensity_eq_integral_smul]
-        · simp [mgf_of_gaussian_integral_eq₂]
+        have : (fun x => (gaussianPDFReal 0 1 x).toNNReal • Real.exp (t * x)) = (fun x => Real.exp (t * x) * gaussianPDFReal 0 1 x) := by
+            ext x
+            rw [NNReal.smul_def]
+            rw [Real.coe_toNNReal (gaussianPDFReal 0 1 x) (gaussianPDFReal_nonneg 0 1 x)]
+            rw [mul_comm (Real.exp (t * x)) (gaussianPDFReal 0 1 x)]
+            rfl
+        · simp [this]
         apply Measurable.real_toNNReal
         exact measurable_gaussianPDFReal 0 1
       _ = ∫ (x : ℝ), Real.exp (t * x) * ((√(2 * Real.pi))⁻¹ * Real.exp (- (x ^ 2) / 2)) := by
         simp [gaussianPDFReal]
       _ = Real.exp (t ^ 2/ 2) * ∫ (x : ℝ), (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2) := by
-        rw [mgf_of_gaussian_integral_eq]
+        have : (fun x => Real.exp (t * x) * ((√(2 * Real.pi))⁻¹ * Real.exp (- (x ^ 2) / 2))) =  (fun x => Real.exp (t ^ 2/ 2) * (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2)) := by
+            ext x
+            simp [mul_sub, sub_eq_zero, mul_assoc, mul_comm, mul_left_comm, ← Real.exp_sub, ← Real.exp_add]
+            field_simp [Real.exp_ne_zero]
+            rw [← Real.exp_add, ← Real.exp_add]
+            field_simp
+            ring
+        rw [this]
         simp [mul_assoc, integral_mul_left]
       _ = Real.exp (t ^ 2 / 2) := by
         have h: ∫ (x : ℝ), (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2) = ∫ (x : ℝ), (gaussianPDFReal t 1 x) := by
@@ -81,17 +80,40 @@ theorem mgf_of_gaussian (X: Ω → ℝ) (hXm: Measurable X) (hX: (Measure.map X 
         ring
         simp only [ne_eq, one_ne_zero, not_false_eq_true]
 
-theorem mgf_of_iid_unit_norm
+-- (mgf α • X) t = (mgf X) (α * t)
+lemma mgf_smul_left (X : Ω → ℝ) (α t : ℝ) : (mgf (α • X)) μ t = (mgf X) μ (t * α) := by
+    rw [mgf, mgf]
+    simp [mul_assoc]
+
+-- mgfs of identically distributed function are equal
+lemma mgf_ident_fun (X Y : Ω → ℝ) (hident : IdentDistrib X Y μ μ) : mgf X μ t = mgf Y μ t := by
+    rw [mgf, mgf]
+    apply IdentDistrib.integral_eq
+    let u := fun x => Real.exp (t * x)
+    have compX : (fun ω ↦ Real.exp (t * X ω)) = u ∘ X := rfl
+    have compY : (fun ω ↦ Real.exp (t * Y ω)) = u ∘ Y := rfl
+    rw [compX, compY]
+    exact IdentDistrib.comp hident (Measurable.exp (measurable_const_mul t))
+
+-- Xᵢ iid => mgf (1/√n ∑ᵢ Xᵢ) t = ((mgf X₀) (t/√n))^n
+theorem mgf_of_iid
 {Y : ℕ → Ω → ℝ} -- sequence of random variables
 {Z : ℕ → Ω → ℝ} -- sum of a given number of random variables
 (h_meas : ∀ (i : ℕ), Measurable (Y i)) -- measurable
 (h_indep : ProbabilityTheory.iIndepFun (fun (i : ℕ) => inferInstance) Y μ) -- independent
 (hident : ∀ (i : ℕ), ProbabilityTheory.IdentDistrib (Y i) (Y 0) μ μ) -- identically distributed
-(h_zero_mean : ∀ i, μ[Y i] = 0) -- zero mean
-(h_unit_variance : ∀ i, μ[(Y i)^2] = 1) -- unit variance
-(Z_def : ∀ n : ℕ, ∀ w : Ω, Z n w = ∑ i ∈ Finset.range n, (Y i w) / (Real.sqrt n)) -- def of Z as the sum of a given number of random variables
-: ∀ n : ℕ, ∀ t : ℝ, mgf (Z n) μ t = (mgf (Y 0) μ (t / Real.sqrt n)) ^ n := by
-  sorry
+(Z_def : ∀ n : ℕ, Z n = (Real.sqrt n)⁻¹ • (∑ i ∈ Finset.range n, (Y i)) ) -- def of Z as the sum of a given number of random variables
+: ∀ n : ℕ, ∀ t : ℝ, mgf (Z n) μ t = (mgf (Y 0) μ (t * (√↑n)⁻¹)) ^ n := by
+    intro n
+    rw [Z_def]
+    intro t
+    rw [mgf_smul_left μ (∑ i ∈ Finset.range n, (Y i)) (Real.sqrt n)⁻¹ t]
+    rw [iIndepFun.mgf_sum h_indep h_meas]
+    have mgf_eq : ∀ i ∈ Finset.range n, (fun i => (mgf (Y i) μ (t * (√↑n)⁻¹))) i = mgf (Y 0) μ (t * (√↑n)⁻¹) := by
+        intro i _
+        exact mgf_ident_fun μ (Y i) (Y 0) (hident i)
+    nth_rw 4 [← Finset.card_range n]
+    exact Finset.prod_eq_pow_card mgf_eq
 
 -- TODO: add little o term
 theorem lemma4
