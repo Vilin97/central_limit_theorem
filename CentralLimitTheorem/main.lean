0ed57e943d642eaa95fe547780024b9e3a0dfbdf
Siyuan Ge <jamesgsy@cs.washington.edu>
2025-01-27 17:40:39 -0800

    updated proof using the pushed theorems in mathlib

--- CentralLimitTheorem/main.lean
+++ CentralLimitTheorem/main.lean
@@ -1,11 +1,12 @@
 import Mathlib.Analysis.SpecialFunctions.Pow.Deriv
-import Mathlib.Probability.Moments
+import Mathlib.Probability.Moments.Basic
 import Mathlib.Probability.Distributions.Gaussian
 import Mathlib.Data.Complex.Exponential
 import Mathlib.Probability.StrongLaw
 
 #check ProbabilityTheory.mgf
 #check ProbabilityTheory.gaussianReal
+#check ProbabilityTheory.mgf_gaussianReal
 
 -- Blueprint: https://www.overleaf.com/read/gbcxfrhjfxqv#a8e535
 
@@ -19,68 +20,83 @@ open MeasureTheory ProbabilityTheory
 variable [MeasurableSpace Ω] (μ: Measure Ω)
 
 -- MGF of standard gaussian is exp(t^2/2)
-theorem mgf_of_gaussian (X: Ω → ℝ) (hXm: Measurable X) (hX: (Measure.map X μ) = (gaussianReal 0 1)) :
-  ∀ t : ℝ, mgf X μ t = Real.exp (t ^ 2 / 2) := by
-    intro t
-    calc
-      mgf X μ t = μ[fun w => Real.exp (t * (X w))] := by
-        simp [mgf]
-      _ = (Measure.map X μ)[fun x => Real.exp (t * x)] := by
-        simp
-        have t₁: AEMeasurable X μ := by exact Measurable.aemeasurable hXm
-        have t₂: AEStronglyMeasurable (fun x ↦ Real.exp (t * x)) (Measure.map X μ) := by
-          apply AEMeasurable.aestronglyMeasurable
-          apply Measurable.aemeasurable
-          apply Real.measurable_exp.comp
-          exact Continuous.measurable (continuous_const.mul continuous_id)
-        rw [MeasureTheory.integral_map t₁ t₂]
-      _ = ∫ (x : ℝ), Real.exp (t * x) * (gaussianPDFReal 0 1 x) := by
-        rw [hX]
-        rw [gaussianReal_of_var_ne_zero 0 one_ne_zero]
-        rw [gaussianPDF_def]
-        simp [ENNReal.ofReal]
-        rw [integral_withDensity_eq_integral_smul]
-        have : (fun x => (gaussianPDFReal 0 1 x).toNNReal • Real.exp (t * x)) = (fun x => Real.exp (t * x) * gaussianPDFReal 0 1 x) := by
-            ext x
-            rw [NNReal.smul_def]
-            rw [Real.coe_toNNReal (gaussianPDFReal 0 1 x) (gaussianPDFReal_nonneg 0 1 x)]
-            rw [mul_comm (Real.exp (t * x)) (gaussianPDFReal 0 1 x)]
-            rfl
-        · simp [this]
-        apply Measurable.real_toNNReal
-        exact measurable_gaussianPDFReal 0 1
-      _ = ∫ (x : ℝ), Real.exp (t * x) * ((√(2 * Real.pi))⁻¹ * Real.exp (- (x ^ 2) / 2)) := by
-        simp [gaussianPDFReal]
-      _ = Real.exp (t ^ 2/ 2) * ∫ (x : ℝ), (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2) := by
-        have : (fun x => Real.exp (t * x) * ((√(2 * Real.pi))⁻¹ * Real.exp (- (x ^ 2) / 2))) =  (fun x => Real.exp (t ^ 2/ 2) * (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2)) := by
-            ext x
-            simp [mul_sub, sub_eq_zero, mul_assoc, mul_comm, mul_left_comm, ← Real.exp_sub, ← Real.exp_add]
-            field_simp [Real.exp_ne_zero]
-            rw [← Real.exp_add, ← Real.exp_add]
-            field_simp
-            ring
-        rw [this]
-        simp [mul_assoc, integral_mul_left]
-      _ = Real.exp (t ^ 2 / 2) := by
-        have h: ∫ (x : ℝ), (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2) = ∫ (x : ℝ), (gaussianPDFReal t 1 x) := by
-          simp [gaussianPDFReal]
-        rw [h, integral_gaussianPDFReal_eq_one]
-        ring
-        simp only [ne_eq, one_ne_zero, not_false_eq_true]
+theorem mgf_of_gaussian (X: Ω → ℝ) (hXm: Measurable X) (hX: (Measure.map X μ) = (gaussianReal 0 1)) : ∀ t : ℝ, mgf X μ t = Real.exp (t ^ 2 / 2) := by
+  intro t
+  rw [ProbabilityTheory.mgf_gaussianReal hX t]
+  have h: (NNReal.toReal 1) = 1 := by
+    exact rfl
+  rw [h]
+  ring_nf
 
+/-
+  intro t
+  calc
+    mgf X μ t = μ[fun w => Real.exp (t * (X w))] := by
+      simp [mgf]
+    _ = (Measure.map X μ)[fun x => Real.exp (t * x)] := by
+      simp
+      have t₁: AEMeasurable X μ := by exact Measurable.aemeasurable hXm
+      have t₂: AEStronglyMeasurable (fun x ↦ Real.exp (t * x)) (Measure.map X μ) := by
+        apply AEMeasurable.aestronglyMeasurable
+        apply Measurable.aemeasurable
+        apply Real.measurable_exp.comp
+        exact Continuous.measurable (continuous_const.mul continuous_id)
+      rw [MeasureTheory.integral_map t₁ t₂]
+    _ = ∫ (x : ℝ), Real.exp (t * x) * (gaussianPDFReal 0 1 x) := by
+      rw [hX]
+      rw [gaussianReal_of_var_ne_zero 0 one_ne_zero]
+      rw [gaussianPDF_def]
+      simp [ENNReal.ofReal]
+      rw [integral_withDensity_eq_integral_smul]
+      have : (fun x => (gaussianPDFReal 0 1 x).toNNReal • Real.exp (t * x)) = (fun x => Real.exp (t * x) * gaussianPDFReal 0 1 x) := by
+          ext x
+          rw [NNReal.smul_def]
+          rw [Real.coe_toNNReal (gaussianPDFReal 0 1 x) (gaussianPDFReal_nonneg 0 1 x)]
+          rw [mul_comm (Real.exp (t * x)) (gaussianPDFReal 0 1 x)]
+          rfl
+      · simp [this]
+      apply Measurable.real_toNNReal
+      exact measurable_gaussianPDFReal 0 1
+    _ = ∫ (x : ℝ), Real.exp (t * x) * ((√(2 * Real.pi))⁻¹ * Real.exp (- (x ^ 2) / 2)) := by
+      simp [gaussianPDFReal]
+    _ = Real.exp (t ^ 2/ 2) * ∫ (x : ℝ), (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2) := by
+      have : (fun x => Real.exp (t * x) * ((√(2 * Real.pi))⁻¹ * Real.exp (- (x ^ 2) / 2))) =  (fun x => Real.exp (t ^ 2/ 2) * (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2)) := by
+          ext x
+          simp [mul_sub, sub_eq_zero, mul_assoc, mul_comm, mul_left_comm, ← Real.exp_sub, ← Real.exp_add]
+          field_simp [Real.exp_ne_zero]
+          rw [← Real.exp_add, ← Real.exp_add]
+          field_simp
+          ring
+      rw [this]
+      simp [mul_assoc, integral_mul_left]
+    _ = Real.exp (t ^ 2 / 2) := by
+      have h: ∫ (x : ℝ), (√(2 * Real.pi))⁻¹ * Real.exp (- ((x - t) ^ 2) / 2) = ∫ (x : ℝ), (gaussianPDFReal t 1 x) := by
+        simp [gaussianPDFReal]
+      rw [h, integral_gaussianPDFReal_eq_one]
+      ring
+      simp only [ne_eq, one_ne_zero, not_false_eq_true]
+-/
+
+/-
 -- (mgf α • X) t = (mgf X) (α * t)
 lemma mgf_smul_left (X : Ω → ℝ) (α t : ℝ) : (mgf (α • X)) μ t = (mgf X) μ (t * α) := by
     simp only [mgf, Pi.smul_apply, smul_eq_mul, mul_assoc]
+-/
 
+/-
 -- mgfs of identically distributed function are equal
-lemma mgf_ident_fun (X Y : Ω → ℝ) (hident : IdentDistrib X Y μ μ) : mgf X μ t = mgf Y μ t := by
-    rw [mgf, mgf]
-    apply IdentDistrib.integral_eq
-    let u := fun x => Real.exp (t * x)
-    have compX : (fun ω ↦ Real.exp (t * X ω)) = u ∘ X := rfl
-    have compY : (fun ω ↦ Real.exp (t * Y ω)) = u ∘ Y := rfl
-    rw [compX, compY]
-    exact IdentDistrib.comp hident (Measurable.exp (measurable_const_mul t))
+lemma mgf_ident_fun (X Y : Ω → ℝ) (hident : IdentDistrib X Y μ μ): mgf X μ t = mgf Y μ t := by
+  rw [mgf, mgf]
+  apply IdentDistrib.integral_eq
+  let u := fun x => Real.exp (t * x)
+  have compX : (fun ω ↦ Real.exp (t * X ω)) = u ∘ X := rfl
+  have compY : (fun ω ↦ Real.exp (t * Y ω)) = u ∘ Y := rfl
+  rw [compX, compY]
+  exact IdentDistrib.comp hident (Measurable.exp (measurable_const_mul t))
+-/
+
+#check mgf_sum_of_identDistrib
+#check ProbabilityTheory.mgf_smul_left
 
 -- Xᵢ iid => mgf (1/√n ∑ᵢ Xᵢ) t = ((mgf X₀) (t/√n))^n
 theorem mgf_of_iid
@@ -88,19 +104,34 @@ theorem mgf_of_iid
 {Z : ℕ → Ω → ℝ} -- sum of a given number of random variables
 (h_meas : ∀ (i : ℕ), Measurable (Y i)) -- measurable
 (h_indep : ProbabilityTheory.iIndepFun (fun (i : ℕ) => inferInstance) Y μ) -- independent
-(hident : ∀ (i : ℕ), ProbabilityTheory.IdentDistrib (Y i) (Y 0) μ μ) -- identically distributed
+(hident : ∀ (i : ℕ), ∀ (j : ℕ), ProbabilityTheory.IdentDistrib (Y i) (Y j) μ μ) -- identically distributed
 (Z_def : ∀ n : ℕ, Z n = (Real.sqrt n)⁻¹ • (∑ i ∈ Finset.range n, (Y i)) ) -- def of Z as the sum of a given number of random variables
-: ∀ n : ℕ, ∀ t : ℝ, mgf (Z n) μ t = (mgf (Y 0) μ (t * (√n)⁻¹)) ^ n := by
-    intro n
-    rw [Z_def]
-    intro t
-    rw [mgf_smul_left μ (∑ i ∈ Finset.range n, (Y i)) (Real.sqrt n)⁻¹ t]
-    rw [iIndepFun.mgf_sum h_indep h_meas]
-    have mgf_eq : ∀ i ∈ Finset.range n, (fun i => (mgf (Y i) μ (t * (√↑n)⁻¹))) i = mgf (Y 0) μ (t * (√↑n)⁻¹) := by
-        intro i _
-        exact mgf_ident_fun μ (Y i) (Y 0) (hident i)
-    nth_rw 4 [← Finset.card_range n]
-    exact Finset.prod_eq_pow_card mgf_eq
+: ∀ n : ℕ, n > 0 → (∀ t : ℝ, mgf (Z n) μ t = (mgf (Y 0) μ ((√n)⁻¹ * t)) ^ n) := by
+  intro n hn t
+  rw [Z_def]
+  rw [ProbabilityTheory.mgf_smul_left]
+  have h0: 0 ∈ (Finset.range n) := by
+    exact Finset.mem_range.mpr hn
+  have h_ident₁ : ∀ i ∈ (Finset.range n), ∀ j ∈ (Finset.range n), ProbabilityTheory.IdentDistrib (Y i) (Y j) μ μ := by
+    intro i j
+    exact fun j a ↦ hident i j
+  rw [mgf_sum_of_identDistrib h_meas h_indep h_ident₁ h0 ((√n)⁻¹ * t)]
+  have : (Finset.range n).card = n := by
+    exact Finset.card_range n
+  rw [this]
+/-
+  intro n
+  rw [Z_def]
+  intro t
+  rw [mgf_smul_left μ (∑ i ∈ Finset.range n, (Y i)) (Real.sqrt n)⁻¹ t]
+  rw [iIndepFun.mgf_sum h_indep h_meas]
+  have mgf_eq : ∀ i ∈ Finset.range n, (fun i => (mgf (Y i) μ (t * (√↑n)⁻¹))) i = mgf (Y 0) μ (t * (√↑n)⁻¹) := by
+      intro i _
+      exact mgf_ident_fun μ (Y i) (Y 0) (hident i)
+  nth_rw 4 [← Finset.card_range n]
+  exact Finset.prod_eq_pow_card mgf_eq
+-/
+
 
 -- TODO: add little o term
 theorem lemma4
